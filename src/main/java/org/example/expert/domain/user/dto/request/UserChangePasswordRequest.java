b745b17a0c80d44a7abe98fd3a0c3333e8fd22a8
seaking110 <seaking110@naver.com>
2025-02-24 20:52:08 +0900

    레벨 1-3 : Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$", message = "새 비밀번호는 숫자와 대문자를 포함해야 합니다.")
+    @Size(min = 9, message = "새 비밀번호는 8자 이상이어야 합니다.")
     @NotBlank
     private String oldPassword;
     @NotBlank
